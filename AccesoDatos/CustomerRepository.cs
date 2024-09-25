3a9364e9d87d0b3428ceb20be78d7a7cafb1e2fa
AndreaN <nm0753032023@unab.edu.sv>
2024-09-24 23:01:42 -0600

    Guia 3

--- AccesoDatos/CustomerRepository.cs
+++ AccesoDatos/CustomerRepository.cs
@@ -31,5 +31,29 @@ namespace AccesoDatos
                 return cliente;
             }
         }
+
+        public Customers ObtenerPorID(string id)
+        {
+            using (var conexion = DataBase.GetSqlConnection())
+            {
+                String SelectForID = "";
+                SelectForID = SelectForID + "SELECT [CustomerID] " + "\n";
+                SelectForID = SelectForID + "      ,[CompanyName] " + "\n";
+                SelectForID = SelectForID + "      ,[ContactName] " + "\n";
+                SelectForID = SelectForID + "      ,[ContactTitle] " + "\n";
+                SelectForID = SelectForID + "      ,[Address] " + "\n";
+                SelectForID = SelectForID + "      ,[City] " + "\n";
+                SelectForID = SelectForID + "      ,[Region] " + "\n";
+                SelectForID = SelectForID + "      ,[PostalCode] " + "\n";
+                SelectForID = SelectForID + "      ,[Country] " + "\n";
+                SelectForID = SelectForID + "      ,[Phone] " + "\n";
+                SelectForID = SelectForID + "      ,[Fax] " + "\n";
+                SelectForID = SelectForID + "  FROM [dbo].[Customers] " + "\n";
+                SelectForID = SelectForID + "  WHERE CustomerID = @CustomerID";
+
+                var cliente = conexion.QueryFirstOrDefault<Customers>(SelectForID, new {CustomerID = id});
+                return cliente;
+            }
+        }
     }
 }
--- DapperDemo/Form1.Designer.cs
+++ DapperDemo/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.dgvCustomers = new System.Windows.Forms.DataGridView();
             this.btnObtenerTodos = new System.Windows.Forms.Button();
+            this.btnObtenerID = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtObtenerID = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +48,7 @@
             // 
             // btnObtenerTodos
             // 
-            this.btnObtenerTodos.Location = new System.Drawing.Point(529, 473);
+            this.btnObtenerTodos.Location = new System.Drawing.Point(1051, 411);
             this.btnObtenerTodos.Name = "btnObtenerTodos";
             this.btnObtenerTodos.Size = new System.Drawing.Size(146, 42);
             this.btnObtenerTodos.TabIndex = 1;
@@ -53,17 +56,47 @@
             this.btnObtenerTodos.UseVisualStyleBackColor = true;
             this.btnObtenerTodos.Click += new System.EventHandler(this.btnObtenerTodos_Click);
             // 
+            // btnObtenerID
+            // 
+            this.btnObtenerID.Location = new System.Drawing.Point(247, 430);
+            this.btnObtenerID.Name = "btnObtenerID";
+            this.btnObtenerID.Size = new System.Drawing.Size(75, 23);
+            this.btnObtenerID.TabIndex = 2;
+            this.btnObtenerID.Text = "Buscar";
+            this.btnObtenerID.UseVisualStyleBackColor = true;
+            this.btnObtenerID.Click += new System.EventHandler(this.btnObtenerID_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cliente ID";
+            // 
+            // txtObtenerID
+            // 
+            this.txtObtenerID.Location = new System.Drawing.Point(133, 430);
+            this.txtObtenerID.Name = "txtObtenerID";
+            this.txtObtenerID.Size = new System.Drawing.Size(108, 22);
+            this.txtObtenerID.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 713);
+            this.Controls.Add(this.txtObtenerID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnObtenerID);
             this.Controls.Add(this.btnObtenerTodos);
             this.Controls.Add(this.dgvCustomers);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +104,9 @@
 
         private System.Windows.Forms.DataGridView dgvCustomers;
         private System.Windows.Forms.Button btnObtenerTodos;
+        private System.Windows.Forms.Button btnObtenerID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtObtenerID;
     }
 }
 
